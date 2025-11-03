b1f3c66d5375b60bd8ff2367b77913f8386a57f2
Swordman51 <51swordman@gmail.com>
2025-11-03 10:25:03 -0800

    Added all constants

--- ECE3_Project_Car_Code.ino
+++ ECE3_Project_Car_Code.ino
@@ -1,6 +1,19 @@
 void setup() {
-  // put your setup code here, to run once:
+  int min[] = {779,	687, 596,	664, 619,	687, 642,	756}; //change if recalibrated
+  int max[] = {1721, 1813, 1904, 1836, 1881, 1813, 1858, 1744};  //change if recalibrated
+  int weights[] = {-15, -14, -12, -8, 8, 12, 14, 15};
 
+  //No Sleep Pins
+  int nSlpLeft = 31;
+  int nSlpRight = 11;
+
+  //PWM Pins
+  int PWMLeft = 40;
+  int PWMRight = 39;
+
+  //Direction Pins
+  int Dir_L = 29;
+  int Dir_R = 30;
 }
 
 void loop() {
